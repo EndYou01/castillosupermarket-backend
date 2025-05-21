24231398f34492bd68e21095445b2be6df990eab
Jorge Senjudo <95231207+EndYou01@users.noreply.github.com>
2025-05-21 01:23:56 -0400

    Cambios en endpoint de obtener productos

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -29,3 +29,61 @@ export interface IGastosExtras {
   fecha: string;
   amount: number;
 }
+
+export interface IProductResponse {
+  items: IProduct[];
+}
+export interface IProduct {
+  id:                  string;
+  handle:              string;
+  reference_id:        null;
+  item_name:           string;
+  description:         null;
+  track_stock:         boolean;
+  sold_by_weight:      boolean;
+  is_composite:        boolean;
+  use_production:      boolean;
+  category_id:         null;
+  components:          any[];
+  primary_supplier_id: null;
+  tax_ids:             any[];
+  modifier_ids:        any[];
+  form:                string;
+  color:               string;
+  image_url:           string;
+  option1_name:        null;
+  option2_name:        null;
+  option3_name:        null;
+  created_at:          Date;
+  updated_at:          Date;
+  deleted_at:          null;
+  variants:            Variant[];
+}
+
+export interface Variant {
+  variant_id:           string;
+  item_id:              string;
+  sku:                  string;
+  reference_variant_id: null;
+  option1_value:        null;
+  option2_value:        null;
+  option3_value:        null;
+  barcode:              null;
+  cost:                 number;
+  purchase_cost:        null;
+  default_pricing_type: string;
+  default_price:        number;
+  stores:               Store[];
+  created_at:           Date;
+  updated_at:           Date;
+  deleted_at:           null;
+}
+
+export interface Store {
+  store_id:           string;
+  pricing_type:       string;
+  price:              number;
+  available_for_sale: boolean;
+  optimal_stock:      null;
+  low_stock:          null;
+}
--- src/productos/productos.controller.ts
+++ src/productos/productos.controller.ts
@@ -2,19 +2,17 @@ import {
   Controller,
   Get,
   InternalServerErrorException,
-  Query,
 } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { IProductResponse } from "src/interfaces/interfaces";
 
 @Controller("productos")
 export class ProductosController {
   private readonly BASE_URL = "https://api.loyverse.com/v1.0";
   private readonly loyverseToken: string;
-  private readonly store_id: string;
 
   constructor(private readonly configService: ConfigService) {
     this.loyverseToken = this.configService.get<string>("LOYVERSE_TOKEN");
-    this.store_id = this.configService.get<string>("STORE_ID");
   }
 
   @Get()
@@ -31,22 +29,22 @@ export class ProductosController {
 
       if (!response.ok) {
         const errorBody: any = await response.json();
-        console.error("❌ Error en respuesta de Loyverse:", errorBody);
         throw new InternalServerErrorException(
           errorBody.errors?.[0]?.details || "Error en la API de Loyverse"
         );
       }
 
-      const data: any = await response.json();
+      const data: IProductResponse = await response.json();
 
-      return {
-        ...data ,
-        itemsNo: data.items.length
-      };
+      return data.items.map((item) => ({
+        item_name: item.item_name,
+        price: item.variants[0]?.default_price || 0,
+        category_id: item.category_id,
+        image_url: item.image_url,
+      }));
     } catch (error) {
-      console.error("🚨 Error al obtener ventas:", error);
       throw new InternalServerErrorException(
-        "No se pudo obtener la información de pro"
+        "No se pudo obtener la información de productos"
       );
     }
   }
